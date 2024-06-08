eae16fae3ddf06e74a436db3014d7b0bda64b64c
SUZ-tsinghua <suz22@mails.tsinghua.edu.cn>
2024-06-08 03:47:45 -0700

    添加了AI vs AI，加了个按钮

--- composite_demo/main.py
+++ composite_demo/main.py
@@ -7,9 +7,13 @@ st.set_page_config(
 )
 
 import demo_tool_m
+from enum import Enum
 
 # Set the title of the demo
 st.title("海龟汤 Demo")
+
+class Mode(str, Enum):
+    AI_AS_HOST, AI_VS_AI = 'AI as Host', 'AI vs AI'
 
 with st.sidebar:
     top_p = st.slider(
@@ -19,14 +23,33 @@ with st.sidebar:
         'temperature', 0.0, 1.5, 0.95, step=0.01
     )
 
-# 输入框
-prompt_text = st.chat_input(
-    '来玩一把海龟汤吧!',
-    key='chat_input',
+tab = st.radio(
+    'Mode',
+    [mode.value for mode in Mode],
+    horizontal=True,
+    label_visibility='hidden',
 )
 
-demo_tool_m.main(
-            prompt_text=prompt_text,
-            top_p=top_p,
-            temperature=temperature,
+match tab:
+    case Mode.AI_AS_HOST:
+        # 输入框
+        prompt_text = st.chat_input(
+            '来玩一把海龟汤吧!',
+            key='chat_input',
         )
+
+        demo_tool_m.main(
+                    prompt_text=prompt_text,
+                    top_p=top_p,
+                    temperature=temperature,
+                )
+    case Mode.AI_VS_AI:
+        with st.sidebar:
+            cols = st.columns(1)
+            generate_bttn = cols[0].button("生成一轮（仅限AI vs AI）", use_container_width=True)
+        if generate_bttn:
+            demo_tool_m.main(
+                    prompt_text="1",
+                    top_p=top_p,
+                    temperature=temperature,
+                )
